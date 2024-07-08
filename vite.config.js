0206d8675ecae8daa88185eaba08fc9e440472f9
David García <davgarcia.dev@gmail.com>
2024-07-08 23:27:33 +0200

    cambios en ficheros

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // Especifica la carpeta base para la aplicación (opcional, por defecto es '/')
+  base: 'https://davgarciadev.github.io/rock-band-site',
+
+  // Configuración específica para el entorno de desarrollo
+ 
+});
