f66f9fc88369cd9f25f35614618e446ce6d28022
David García <davgarcia.dev@gmail.com>
2024-07-11 00:06:42 +0200

    Se sustituyen elementos multimedia por otros más adecuados

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   // Especifica la carpeta base para la aplicación (opcional, por defecto es '/')
-  base: 'https://davgarciadev.github.io/rock-band-site',
+  base: ''
 
   // Configuración específica para el entorno de desarrollo
  
